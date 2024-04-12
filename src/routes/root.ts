d0fe772f03cccce24dca6bf6e980ab282bf65009
tobichimaru <kzabelova@gmail.com>
2024-04-12 18:27:26 +0200

    move reviews

--- src/routes/root.ts
+++ src/routes/root.ts
@@ -1,8 +1,17 @@
 import { FastifyPluginAsync } from 'fastify'
+// import { EAS } from "@ethereum-attestation-service/eas-sdk";
 
 const root: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
     return { root: true }
+  })
+
+  fastify.get('/reviews', async function (request, reply) {
+    return 'this is an example'
+  })
+
+  fastify.post('/reviews', async function (request, reply) {
+    return 'this is an example'
   })
 }
 
