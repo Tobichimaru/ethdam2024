ee53b802b0a0111d2cdc182f4a260967dfbba415
tobichimaru <kzabelova@gmail.com>
2024-04-12 18:25:09 +0200

    add reviews

--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -1,7 +1,17 @@
 import { FastifyPluginAsync } from "fastify"
+import { EAS } from "@ethereum-attestation-service/eas-sdk";
+
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
+    return 'this is an example'
+  })
+
+  fastify.get('/Reviews', async function (request, reply) {
+    return 'this is an example'
+  })
+
+  fastify.post('/Reviews', async function (request, reply) {
     return 'this is an example'
   })
 }
