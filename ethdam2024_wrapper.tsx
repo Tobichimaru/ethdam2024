3a456a86244be4393f8e879bf8307ff186b3e974
tobichimaru <kzabelova@gmail.com>
2024-04-14 11:52:48 +0200

    update wrapper

--- ethdam2024_wrapper.tsx
+++ ethdam2024_wrapper.tsx
@@ -2,15 +2,15 @@ const Theme = styled.div`
   height: 100%;
 `;
 
-// const LayoutContainer = styled.div`
-//   display: grid;
-//   grid-template-columns: 1fr 2fr;
-//   gap: 20px;
-// `;
+const LayoutContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  gap: 20px;
+`;
 
 const user = Ethers.send("eth_requestAccounts", [])[0];
 const [viewMode, setViewMode] = useState("list"); // 'list' or 'graph'
-const [attestations, setAttestations] = useState([]);
+const [reviews, setReviews] = useState([]);
 
 const toggleView = () => {
   if (viewMode === "list") {
@@ -24,14 +24,20 @@ const shortenAddress = (address) => {
   return `0x${address.slice(2, 7)}...${address.slice(-5)}`;
 };
 
-const GQL_ENDPOINT = "https://sepolia.easscan.org/graphql";
-const SCHEMA_ID =
-  "0x6fe41fc5a5c39368d2aa147368558ffa101c023136e60a84ef05281823ea1d4d";
+const decodeData = (decodedDataJson) => {
+  return JSON.parse(decodedDataJson).reduce((data, field) => {
+    data[field.name] = field.value.value;
+    return data;
+  }, {});
+};
 
-// TODO: Remove default recipient
-const recipient =
-  props.recipient ?? "0x58C8E31bE33DB76B60156276e4abAC096B803a0A";
-const attestationsResponse = fetch(GQL_ENDPOINT, {
+const GQL_ENDPOINT = "https://sepolia.easscan.org/graphql";
+const REVIEWS_SCHEMA_ID =
+  "0x6fe41fc5a5c39368d2aa147368558ffa101c023136e60a84ef05281823ea1d4d";
+const LIKES_SCHEMA_ID =
+  "0x9622d675e8d804bd19e28c6dd268b15a55dc34f19735e06cced73f1ba93c4a6c";
+
+const reviewsResponse = fetch(GQL_ENDPOINT, {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
@@ -42,57 +48,106 @@ const attestationsResponse = fetch(GQL_ENDPOINT, {
     variables: {
       where: {
         schemaId: {
-          equals: SCHEMA_ID,
+          equals: REVIEWS_SCHEMA_ID,
         },
-        recipient: recipient ? { equals: recipient } : undefined,
+        recipient: props.recipient ? { equals: props.recipient } : undefined,
       },
     },
   }),
 });
 
-if (!attestationsResponse.ok) {
-  return <div>Failed to fetch reviews</div>;
+if (!reviewsResponse.ok) {
+  return <h1>Loading...</h1>;
 }
 
-const attestationsData = attestationsResponse.body.data.attestations.map(
-  (a) => {
-    const prettifiedData = JSON.parse(a.decodedDataJson).reduce(
-      (data, field) => {
-        data[field.name] = field.value.value;
-        return data;
+const reviewsData = reviewsResponse.body.data.attestations.map((a) => {
+  return {
+    ...a,
+    attesterShortAddress: shortenAddress(a.attester),
+    recipientShortAddress: shortenAddress(a.recipient),
+    decodedDataJson: undefined,
+    data: decodeData(a.decodedDataJson),
+  };
+});
+
+//
+const likesResponse = fetch(GQL_ENDPOINT, {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    query:
+      "query($likesWhere: AttestationWhereInput) { attestations(where: $likesWhere) { decodedDataJson }}",
+    variables: {
+      likesWhere: {
+        AND: [
+          {
+            schemaId: {
+              equals: LIKES_SCHEMA_ID,
+            },
+          },
+          {
+            OR: [...new Set(reviewsData.map((r) => r.id))].map((reviewId) => ({
+              decodedDataJson: {
+                contains: `\"value\":\"${reviewId}\"`,
+              },
+            })),
+          },
+        ],
       },
-      {}
-    );
+    },
+  }),
+});
+
+if (!likesResponse.ok) {
+  return <h1>Loading...</h1>;
+}
+
+const likesData = likesResponse.body.data.attestations
+  .map((a) => {
     return {
-      ...a,
-      attesterShortAddress: shortenAddress(a.attester),
-      recipientShortAddress: shortenAddress(a.recipient),
       decodedDataJson: undefined,
-      data: prettifiedData,
-      // TODO: Get upvotes and downvotes attestations separately
-      upvotes: 10,
-      downvotes: 4,
+      data: decodeData(a.decodedDataJson),
     };
-  }
-);
+  })
+  .reduce((likesByReviewIds, likeEntry) => {
+    const currentUpvotes =
+      likesByReviewIds[likeEntry.data.attestationId]?.upvotes ?? 0;
+    const currentDownvotes =
+      likesByReviewIds[likeEntry.data.attestationId]?.downvotes ?? 0;
+    const isUpvote = likeEntry.data.upvote === true;
+    likesByReviewIds[likeEntry.data.attestationId] = {
+      upvotes: currentUpvotes + (isUpvote ? 1 : 0),
+      downvotes: currentDownvotes + (isUpvote ? 0 : 1),
+    };
+    return likesByReviewIds;
+  }, {});
+
+reviewsData = reviewsData.map((r) => ({
+  ...r,
+  upvotes: likesData[r.id]?.upvotes ?? 0,
+  downvotes: likesData[r.id]?.downvotes ?? 0,
+}));
 
 useEffect(() => {
-  setAttestations(attestationsData);
-}, [attestationsData]);
+  setReviews(reviewsData);
+}, [reviewsData]);
 
 return (
   <Theme>
-    {/* {user || ""} */}
-    <Widget src="silent_ssh.testnet/widget/walletconnect_testnet" />
-    <button onClick={toggleView}>Toggle View</button>
     <Widget src="silent_ssh.testnet/widget/ethdam2024_eas_fe" />
+    <LayoutContainer>
+      <Widget src="silent_ssh.testnet/widget/walletconnect_testnet" />
+      {/* <button onClick={toggleView}>Toggle View</button> */}
+    </LayoutContainer>
     <Widget
-      src="silent_ssh.testnet/widget/ethdam2024_reviews_graph"
-      props={{ attestations }}
+      src="ilyamatsuev.testnet/widget/ethdam2024_reviews_list"
+      props={{ attestations: reviews }}
     />
     <Widget
-      src="silent_ssh.testnet/widget/ethdam2024_reviews_list"
-      props={{ attestations }}
+      src="silent_ssh.testnet/widget/ethdam2024_reviews_graph"
+      props={{ attestations: reviews }}
     />
   </Theme>
 );
